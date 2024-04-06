ab2dc62fad10426cac28d0602a03ba0b16706beb
Kadir Çimen <kadircimen88@hotmail.com>
2024-04-06 03:40:15 +0300

    UI developments

---
+++ BemCase.Web/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BemCase.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return Content("OK");
+        }
+    }
+}
---
+++ BemCase.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BemCase.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
